7809c6814d4d0ac5f87b728e7647dc30a8ecd6e5
gidwikintan <gidwikintan@gmail.com>
2021-10-22 05:02:34 +0700

    add splash screen

---
+++ src/pages/splash/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Splash = () => {
+  return (
+    <View style={styles.page}>
+      <Text style={styles.title}>My Doctor</Text>
+    </View>
+  );
+};
+
+export default Splash;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#112340',
+    marginTop: 20,
+  },
+});
